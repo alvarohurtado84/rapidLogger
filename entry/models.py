321ff69053235666c21d9cecc66d6f125aee1c99
Alvaro Hurtado <alvaro@dubizzle.com>
2015-03-01 23:03:37 +0400

    [add_app_models_structure] Add models for entries.

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    """Define the basic behaviors for bullets."""
+
+    title = models.CharField()
+
+    irrelevant = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField()
+    accomplish_at = models.DateTimeField()
+
+
+class Task(Entry):
+    """Define a Task bullet."""
+
+    done = models.BooleanField(default=False)
+    done_at = models.DateTimeField()
+
+    migrated_from = models.OneToOneField('Task',
+                                         related_name='migrated_to')
+
+
+class Event():
+    """Define an Event bullet."""
+
+    pass
+
+
+class Note():
+    """Define a Note bullet."""
+
+    pass
